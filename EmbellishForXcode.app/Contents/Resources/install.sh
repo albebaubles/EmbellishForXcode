a4a3fa0382585cbeed930882804f6af513469be4
Al Corbett <al@albebaubles.com>
2020-11-11 09:20:09 -0800

    cleanup

--- EmbellishForXcode.app/Contents/Resources/install.sh
+++ EmbellishForXcode.app/Contents/Resources/install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-set -euo pipefail
-
-url=https://raw.githubusercontent.com/albebaubles/EmbellishForXcode/master/Embellish/scripts/Embellish.scpt
-SCRIPT_DIR="${HOME}/Library/Application Scripts/com.albebaubles.EmbellishForXcode.Embellish"
-
-mkdir -p "${SCRIPT_DIR}"
-curl $url -o "${SCRIPT_DIR}/Embellish.scpt"
+set -euo pipefail && url="https://albebaubles.com/wp-content/uploads/2020/09/Embellish.txt" && SCRIPT_DIR="${HOME}/Library/Application Scripts/com.albebaubles.EmbellishForXcode" && mkdir -p "${SCRIPT_DIR}" && curl $url -o "${SCRIPT_DIR}/Embellish.scpt"
 
